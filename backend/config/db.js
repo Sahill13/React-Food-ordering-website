78f3c8e4182dc92b7d8ce36917da1196e1093f64
Sahill13 <162485804+Sahill13@users.noreply.github.com>
2024-12-07 14:34:03 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-      await mongoose.connect('mongodb+srv://sahilgupta130602:Sahilkingg1@cluster0.d8a2z.mongodb.net/mydatabase?retryWrites=true&w=majority');
+      await mongoose.connect('mongodb+srv://YourName:YourPassword@cluster0.d8a2z.mongodb.net/mydatabase?retryWrites=true&w=majority');
       console.log('DB Connected');
   };
 
